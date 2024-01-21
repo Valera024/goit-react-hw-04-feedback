c6e015700c3eba0e2849d46730a60ae2cb5c3f64
Valera <104449560+Valera024@users.noreply.github.com>
2024-01-21 12:41:24 +0200

    1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,6 @@ const App = () => {
         }))
   }
   
-    // const { good, neutral, bad } = this.state;
         return (
             <>
                 <Section title="Please leave feedback">
